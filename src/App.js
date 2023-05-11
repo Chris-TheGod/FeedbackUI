555a9d6bd2f579d095fa7a56182b7fe796d1deb2
Kristjan Lääne <xonproject@gmail.com>
2023-05-11 19:29:18 +0300

    started using context

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FeedbackData from "./data/FeedbackData";
 import { FeedbackStats } from "./components/FeedbackStats";
 import { FeedbackForm } from "./components/FeedbackForm";
 import { AboutPage } from "./pages/AboutPage";
+import { FeedbackProvider } from "./context/FeedbackContext";
 import { AboutIconLink } from "./components/AboutIconLink";
 
 export const App = () => {
@@ -25,29 +26,31 @@ export const App = () => {
   };
 
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList
-                  feedback={feedback}
-                  handleDelete={deleteFeedback}
-                />
-              </>
-            }
-          ></Route>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
 
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-        <AboutIconLink />
-      </div>
-    </Router>
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 };
